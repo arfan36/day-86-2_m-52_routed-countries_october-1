8bc7daa9424cbd79b10b861f880ed3ba21745e00
arfan36 <arfanur36@gmail.com>
2022-10-06 18:33:09 +0600

    52-9 Module Summary and Recap React Route for Rest Countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Countries = () => {
+    const countries = useLoaderData();
     return (
         <div>
-            <h2>All Countries</h2>
+            <h2>All Countries {countries.length}</h2>
+            {
+                countries.map(country =>
+                    <li key={country.alpha3Code}>
+                        <Link to={`/country/${country.name}`} >{country.name}</Link>
+                    </li>)
+            }
         </div>
     );
 };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Countries from "../components/Countries";
+import Country from "../components/Country";
 
 export const router = createBrowserRouter([
-    { path: '/', element: <Countries></Countries> }
+    {
+        path: '/',
+        loader: async () => {
+            return fetch('https://restcountries.com/v2/all');
+        },
+        element: <Countries></Countries>
+    },
+    {
+        path: '/country/:name',
+        loader: async ({ params }) => {
+            return fetch(`https://restcountries.com/v2/name/${params.name}`);
+        },
+        element: <Country></Country>
+    }
 ]);
